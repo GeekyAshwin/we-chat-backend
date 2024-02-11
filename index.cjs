dabc89f6ed619446d5d8a640169d3e151750d016
Ashwiner <techmindashwin78@gmail.com>
2024-02-11 17:03:11 +0530

    installed packages and implemented socket io

---
+++ index.cjs
@@ -0,0 +1,12 @@
+const { Server } = require('socket.io')
+
+const io = new Server({cors: {origin: '*'}});
+
+io.on('connection', (socket) => {
+    console.log('connected');
+    socket.on('message', (data) => {
+        socket.broadcast.emit('received', {data: data, 'message' : 'Message sent to server.'});  //this code sending data from server to client
+    })
+})
+
+io.listen(3000);
