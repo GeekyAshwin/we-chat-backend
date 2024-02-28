233323fef38243eacefd11a1deda7d5fb595b76d
Ashwiner <techmindashwin78@gmail.com>
2024-02-28 20:47:25 +0530

    handle cors

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ const secretKey = 'your_secret_key';
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 });
 app.use(express.json());
 
