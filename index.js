74a40c13c6899ff7e242b6a0b8ff37cf78fe0561
Ashwiner <techmindashwin78@gmail.com>
2024-02-28 23:45:17 +0530

    added headers for cors

--- index.js
+++ index.js
@@ -12,11 +12,30 @@ const app = express();
 const secretKey = 'your_secret_key';
 
 app.use(express.urlencoded({ extended: true }));
+app.user
 app.use(cors({
   origin: 'https://chatwiner.netlify.app',
 }));
 app.use(express.json());
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://chatwiner.netlify.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // create new server
 const server = new Server({
   cors: { origin: 'https://chatwiner.netlify.app' }
