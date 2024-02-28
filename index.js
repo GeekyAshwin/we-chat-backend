15508a83ce8000695f1eb55f38c0ea51471c4171
Ashwiner <techmindashwin78@gmail.com>
2024-02-28 20:36:42 +0530

    handle cors

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ const secretKey = 'your_secret_key';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 
 // create new server
